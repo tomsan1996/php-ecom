390e735e2080aaf4290a47a55eb1175cebfe97de
tomsan1996 <117666363+tomsan1996@users.noreply.github.com>
2022-11-18 19:44:51 +0700

    Update User.php

--- classes/User.php
+++ classes/User.php
@@ -19,7 +19,7 @@ class User{
 
                 if($this->find($user)){
                     $this->_isLoggedIn = true;
-                }else{
+                }else{  
                     // process logout
                 }
             }
